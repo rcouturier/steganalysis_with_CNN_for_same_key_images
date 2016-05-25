8e810c7283a0ff8489b25bfea326d54f71e9bbfb
couturie <couturie@extinction>
2016-05-25 21:52:49 +0200

    add the download script for images

---
+++ images/download.sh
@@ -0,0 +1,17 @@
+wget -c http://info.iut-bm.univ-fcomte.fr/staff/couturie/Code/DL/models/models/cover_jpg.tgz
+wget -c http://info.iut-bm.univ-fcomte.fr/staff/couturie/Code/DL/models/models/cover_pgm.tgz
+wget -c http://info.iut-bm.univ-fcomte.fr/staff/couturie/Code/DL/models/models/stego_hugo_0.1.tgz
+wget -c http://info.iut-bm.univ-fcomte.fr/staff/couturie/Code/DL/models/models/stego_hugo_0.4.tgz
+wget -c http://info.iut-bm.univ-fcomte.fr/staff/couturie/Code/DL/models/models/stego_uniward_0.1.tgz
+wget -c http://info.iut-bm.univ-fcomte.fr/staff/couturie/Code/DL/models/models/stego_uniward_0.4.tgz
+wget -c http://info.iut-bm.univ-fcomte.fr/staff/couturie/Code/DL/models/models/stego_wow_0.1.tgz
+wget -c http://info.iut-bm.univ-fcomte.fr/staff/couturie/Code/DL/models/models/stego_wow_0.4.tgz
+
+tar ztf cover_jpg.tgz
+tar ztf cover_pgm.tgz
+tar ztf stego_hugo_0.1.tgz
+tar ztf stego_hugo_0.4.tgz
+tar ztf stego_uniward_0.1.tgz
+tar ztf stego_uniward_0.4.tgz
+tar ztf stego_wow_0.1.tgz
+tar ztf stego_wow_0.4.tgz
