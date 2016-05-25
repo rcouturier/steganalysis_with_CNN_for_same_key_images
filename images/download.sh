ef8ebf76750360a41ee12dfd511daf8b22526d54
couturie <couturie@extinction>
2016-05-25 22:02:16 +0200

    correction of the script again

--- images/download.sh
+++ images/download.sh
@@ -7,11 +7,11 @@ wget -c http://info.iut-bm.univ-fcomte.fr/staff/couturie/Code/DL/images/stego_un
 wget -c http://info.iut-bm.univ-fcomte.fr/staff/couturie/Code/DL/images/stego_wow_0.1.tgz
 wget -c http://info.iut-bm.univ-fcomte.fr/staff/couturie/Code/DL/images/stego_wow_0.4.tgz
 
-tar ztf cover_jpg.tgz
-tar ztf cover_pgm.tgz
-tar ztf stego_hugo_0.1.tgz
-tar ztf stego_hugo_0.4.tgz
-tar ztf stego_uniward_0.1.tgz
-tar ztf stego_uniward_0.4.tgz
-tar ztf stego_wow_0.1.tgz
-tar ztf stego_wow_0.4.tgz
+tar zxf cover_jpg.tgz
+tar zxf cover_pgm.tgz
+tar zxf stego_hugo_0.1.tgz
+tar zxf stego_hugo_0.4.tgz
+tar zxf stego_uniward_0.1.tgz
+tar zxf stego_uniward_0.4.tgz
+tar zxf stego_wow_0.1.tgz
+tar zxf stego_wow_0.4.tgz
