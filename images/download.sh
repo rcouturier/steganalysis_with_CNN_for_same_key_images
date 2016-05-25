a532b0b5a89c50098a876cb02410e2f11c31c6e5
couturie <couturie@extinction>
2016-05-25 21:56:43 +0200

    correction of the download script for images

--- images/download.sh
+++ images/download.sh
@@ -1,11 +1,11 @@
-wget -c http://info.iut-bm.univ-fcomte.fr/staff/couturie/Code/DL/models/models/cover_jpg.tgz
-wget -c http://info.iut-bm.univ-fcomte.fr/staff/couturie/Code/DL/models/models/cover_pgm.tgz
-wget -c http://info.iut-bm.univ-fcomte.fr/staff/couturie/Code/DL/models/models/stego_hugo_0.1.tgz
-wget -c http://info.iut-bm.univ-fcomte.fr/staff/couturie/Code/DL/models/models/stego_hugo_0.4.tgz
-wget -c http://info.iut-bm.univ-fcomte.fr/staff/couturie/Code/DL/models/models/stego_uniward_0.1.tgz
-wget -c http://info.iut-bm.univ-fcomte.fr/staff/couturie/Code/DL/models/models/stego_uniward_0.4.tgz
-wget -c http://info.iut-bm.univ-fcomte.fr/staff/couturie/Code/DL/models/models/stego_wow_0.1.tgz
-wget -c http://info.iut-bm.univ-fcomte.fr/staff/couturie/Code/DL/models/models/stego_wow_0.4.tgz
+wget -c http://info.iut-bm.univ-fcomte.fr/staff/couturie/Code/DL/images/cover_jpg.tgz
+wget -c http://info.iut-bm.univ-fcomte.fr/staff/couturie/Code/DL/images/cover_pgm.tgz
+wget -c http://info.iut-bm.univ-fcomte.fr/staff/couturie/Code/DL/images/stego_hugo_0.1.tgz
+wget -c http://info.iut-bm.univ-fcomte.fr/staff/couturie/Code/DL/images/stego_hugo_0.4.tgz
+wget -c http://info.iut-bm.univ-fcomte.fr/staff/couturie/Code/DL/images/stego_uniward_0.1.tgz
+wget -c http://info.iut-bm.univ-fcomte.fr/staff/couturie/Code/DL/images/stego_uniward_0.4.tgz
+wget -c http://info.iut-bm.univ-fcomte.fr/staff/couturie/Code/DL/images/stego_wow_0.1.tgz
+wget -c http://info.iut-bm.univ-fcomte.fr/staff/couturie/Code/DL/images/stego_wow_0.4.tgz
 
 tar ztf cover_jpg.tgz
 tar ztf cover_pgm.tgz
